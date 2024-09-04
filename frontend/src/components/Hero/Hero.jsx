80533837f4245370db20f5068e8f00a7178ef28a
SouravKumbhakar <souravkumbhakar90@gmail.com>
2024-09-04 19:28:29 +0530

    add initial

---
+++ frontend/src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="hero-container">
+            <h2>Order your favourite food here</h2>
+            <p>Chose a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expeertise.Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+import Hero from '../../components/Hero/Hero'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero/>
+      <ExploreMenu/>
+    </div>
+  )
+}
+
+export default Home
